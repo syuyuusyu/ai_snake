decded9e4ca2002943adb7979f220a9cbdd665c4
syuyuusyu <syuyuusyu@gmail.com>
2024-07-25 15:16:59 +0800

    inial

--- test.py
+++ test.py
@@ -1,14 +1,7 @@
-import numpy as np
+board_size = 10
+play_ground = []
 
-# 假设 play_ground 是一个 20x20 的二维数组
-play_ground = np.zeros((20, 20))
-
-# 假设当前方向是 "上"，则方向编码为 [1, 0, 0, 0]
-direction = [1, 0, 0, 0]
-
-# 将方向信息扩展为与 play_ground 形状匹配的矩阵
-direction_info = np.full(play_ground.shape, direction)
-
-# 将 play_ground 和 direction_info 组合为输入
-input_data = np.stack([play_ground, direction_info], axis=-1) 
-print(input_data)
+while True:
+    for i in range(0,board_size):
+        play_ground[i] = [0]* board_size
+    print(len(play_ground))
--- train.py
+++ train.py
@@ -6,8 +6,7 @@ import random
 
 def predict(model, data_channel, device: torch.device = 'cpu') -> str:
     # 将输入数据转换为 PyTorch 张量，并移动到指定设备
-    input_data = torch.tensor(data_channel, dtype=torch.float32).unsqueeze(0).to(device)
-    print(input_data.shape)
+    input_data = torch.tensor(data_channel, dtype=torch.float32).unsqueeze(0).unsqueeze(1).to(device)
 
     # 确保模型在指定设备上，并设置为评估模式
     model.to(device)
@@ -47,12 +46,11 @@ def train(model, optimizer, loss_fn, memory, gamma, batch_size, device):
     
     # 计算损失
     loss = loss_fn(current_q_values, target_q_values)
-    
+    print(loss)
     # 优化模型参数
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
-    
     return loss.item()
 
 
