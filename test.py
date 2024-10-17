2822979696dfd409e09b43674f39e057a4bda8e8
syuyuusyu <syuyuusyu@gmail.com>
2024-10-17 16:04:50 +0800

    mac

--- test.py
+++ test.py
@@ -1,41 +1,42 @@
-import subprocess
-import re
+import torch
+import sb3_contrib
+import stable_baselines3
+import gym
+from typing import Tuple,Deque,List
+from collections import deque
 
-def ping_domain(domain):
-    try:
-        # 执行 ping 命令并捕获输出
-        result = subprocess.run(['ping', '-c', '1', domain], stdout=subprocess.PIPE, text=True)
-        output = result.stdout
+from sb3_contrib import MaskablePPO
+from sb3_contrib.common.wrappers import ActionMasker
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.vec_env import DummyVecEnv, VecMonitor,SubprocVecEnv
+from stable_baselines3.common.env_checker import check_env
+import numpy as np
+import math
+import random
 
-        # 使用正则表达式提取 IP 地址
-        ip_match = re.search(r'PING [^\(]*\(([\d\.]+)\)', output)
-        if ip_match:
-            ip_address = ip_match.group(1)
-        else:
-            ip_address = None
+print(torch.__version__)
 
-        # 判断是否能 ping 通，根据 returncode 来判断
-        if result.returncode == 0:
-            return ip_address, True  # IP 和能 ping 通
-        else:
-            return ip_address, False  # IP 和不能 ping 通
+# 导入你定义的 SnakeEnv 类
+from game_env import SnakeEnv
 
-    except subprocess.CalledProcessError as e:
-        print(f"Error executing ping command: {e}")
-        return None, False  # 出现异常，不能 ping 通
-
-# 示例
-domains = ["c50s1.portablesubmarines.com","c50s2.portablesubmarines.com","c50s3.portablesubmarines.com","c50s4.portablesubmarines.com","c50s5.portablesubmarines.com","c50s801.portablesubmarines.com"]
-for domain in domains:
-    ip, is_reachable = ping_domain(domain)
-
-    if is_reachable:
-        print(f"Ping {domain} succeeded. IP: {ip}")
-    else:
-        print(f"Ping {domain} failed. IP: {ip if ip else 'Not available'}")
+device = 'cpu'
+if torch.cuda.is_available():
+    device = 'cuda'
+elif torch.backends.mps.is_available():
+    device = 'mps'
 
 
-arr = [1,2,3]
-a = arr.index(1)
+
+a = [1,2,3,4,5,6,7]
+
+# 3 -> 1
+# 4 -> 1,2
+# 5 -> 2
+# 6 -> 2,3
+# 7 -> 3
+# 8 -> 3,4
+
+
+a = [[0]*10]*10
 print(a)
 
