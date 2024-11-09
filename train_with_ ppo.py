8292fdfd0d1fc886df27b28ed854b46b8f5caf3e
yu s <syuyuusyu@gmail.com>
2024-11-09 10:20:48 +0800

    pc

--- train_with_ ppo.py
+++ train_with_ ppo.py
@@ -59,9 +59,9 @@ class MonitorCallback(BaseCallback):
         for train_info in train_info_list:
             for key in average_info.keys():
                 average_info[key] += train_info[key]
-            dic = train_info['repeat_map']
-            for k,v in dic.items():
-                repeat_map[k] = repeat_map[k]+v
+            # dic = train_info['repeat_map']
+            # for k,v in dic.items():
+            #     repeat_map[k] = repeat_map[k]+v
                 #print(repeat_map)
                 
                 
@@ -128,14 +128,14 @@ def load():
     #lr_schedule = schedule_fn(5e-4, 2.5e-6)
     lr_schedule = schedule_fn(5e-5, 1e-6)
     clip_range_schedule = schedule_fn(0.150, 0.025)
-    model = MaskablePPO.load("pth/final_13.zip", env=env, device=device)
-    model.gamma=0.97
+    model = MaskablePPO.load("pth/final_21.zip", env=env, device=device)
+    model.gamma=0.98
     model.learning_rate = lr_schedule
     model.clip_range = clip_range_schedule
     model.ent_coef = 0
     info_callback = MonitorCallback() 
     model.learn(total_timesteps=2e8,callback=[info_callback])
-    model.save('pth/final_14')
+    model.save('pth/final_22')
     env.close()
 
 if __name__ == '__main__':
