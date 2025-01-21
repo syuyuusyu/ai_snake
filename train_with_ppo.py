9a8817c76442dbad51c89295eee593d943774049
syuyuusyu <syuyuusyu@gmail.com>
2025-01-21 13:32:17 +0800

    mac

--- train_with_ppo.py
+++ train_with_ppo.py
@@ -25,7 +25,7 @@ elif torch.backends.mps.is_available():
 
 
 repeat_map = defaultdict(int)
-bfs_intensity = 0
+bfs_intensity = 1
 
 def make_env(seed=0,board_size=10):
     def _init():
@@ -66,8 +66,6 @@ class MonitorCallback(BaseCallback):
             #     repeat_map[k] = repeat_map[k]+v
                 #print(repeat_map)
                 
-                
-
         # 计算平均值
         for key in average_info.keys():
             average_info[key] /= num_envs
@@ -124,14 +122,14 @@ def main():
 
 def load():
     seed_set = set()
-    while len(seed_set) < 64:
+    while len(seed_set) < 128:
         seed_set.add(random.randint(0,1e7))
     env = SubprocVecEnv([make_env(seed,board_size) for seed in seed_set])
     #lr_schedule = schedule_fn(5e-4, 2.5e-6)asdfsadfsdaf
     lr_schedule = schedule_fn(5e-5, 1e-6)
     #clip_range_schedule = schedule_fn(0.150, 0.025)
     clip_range_schedule = schedule_fn(5e-5, 1e-6)
-    model = MaskablePPO.load("pth/final_33.zip", env=env, device=device)
+    model = MaskablePPO.load("pth/final_51.zip", env=env, device=device)
     model.gamma=0.98
     model.learning_rate = lr_schedule
     model.clip_range = clip_range_schedule
@@ -139,8 +137,8 @@ def load():
     #model.n_steps = 2048
     model.batch_size = 512 * 8
     info_callback = MonitorCallback() 
-    model.learn(total_timesteps=2e8,callback=[info_callback])
-    model.save('pth/final_34')
+    model.learn(total_timesteps=1e8,callback=[info_callback])
+    model.save('pth/final_52')
     env.close()
 
 if __name__ == '__main__':
