5744af1d48f34637ff6a54bb5fd9aa8767528948
syuyuusyu <syuyuusyu@gmail.com>
2025-03-24 16:19:02 +0800

    conda

--- train_with_ppo.py
+++ train_with_ppo.py
@@ -27,7 +27,7 @@ elif torch.backends.mps.is_available():
 repeat_map = defaultdict(int)
 bfs_intensity = 1
 
-def make_env(seed=0,board_size=10):
+def make_env(seed=0,board_size=12):
     def _init():
         env = SnakeEnv(seed=seed,board_size=board_size, silent_mode=True,bfs_intensity = bfs_intensity)
         env = ActionMasker(env, SnakeEnv.mask_fn)
@@ -125,11 +125,11 @@ def load():
     while len(seed_set) < 128:
         seed_set.add(random.randint(0,1e7))
     env = SubprocVecEnv([make_env(seed,board_size) for seed in seed_set])
-    #lr_schedule = schedule_fn(5e-4, 2.5e-6)asdfsadfsdaf
-    lr_schedule = schedule_fn(5e-5, 1e-6)
+    #lr_schedule = schedule_fn(5e-4, 2.5e-6)
+    lr_schedule = schedule_fn(2e-5, 1e-6)
     #clip_range_schedule = schedule_fn(0.150, 0.025)
-    clip_range_schedule = schedule_fn(5e-5, 1e-6)
-    model = MaskablePPO.load("pth/stable_1.zip", env=env, device=device)
+    clip_range_schedule = schedule_fn(2e-5, 1e-6)
+    model = MaskablePPO.load("pth/stable_3.zip", env=env, device=device)
     model.gamma=0.98
     model.learning_rate = lr_schedule
     model.clip_range = clip_range_schedule
@@ -138,7 +138,7 @@ def load():
     model.batch_size = 512 * 8
     info_callback = MonitorCallback() 
     model.learn(total_timesteps=1e8,callback=[info_callback])
-    model.save('pth/stable_2')
+    model.save('pth/stable_4')
     env.close()
 
 if __name__ == '__main__':
